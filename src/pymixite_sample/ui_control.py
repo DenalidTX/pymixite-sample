c4fa2c9c892ba0b049feb50e7c961ca1a30b122f
DenalidTX <DenalidTX@gmail.com>
2022-04-25 20:30:14 -0500

    Updated sample to react to changes in grid size. The status bar will indicate invalid grid configurations.

--- src/pymixite_sample/ui_control.py
+++ src/pymixite_sample/ui_control.py
@@ -3,7 +3,7 @@ from PyQt5.QtGui import QPainter, QPolygonF
 from PyQt5.QtWidgets import QGraphicsScene, QLineEdit
 from mixite import HexagonImpl, Point
 from mixite.coord import CubeCoordinate
-from mixite.builder import GridControlBuilder, GridControl
+from mixite.builder import GridControlBuilder, GridControl, GridBuildException
 
 
 class UIInitializer:
@@ -41,6 +41,8 @@ class UIInitializer:
 
         self.root_widget.layoutComboBox.currentIndexChanged.connect(self.redraw_grid)
         self.root_widget.orientationComboBox.currentIndexChanged.connect(self.redraw_grid)
+        self.root_widget.gridWidthBox.valueChanged.connect(self.redraw_grid)
+        self.root_widget.gridHeightBox.valueChanged.connect(self.redraw_grid)
 
     def mouse_move_event(self, event):
         self.root_widget.canvasXBox.setText(str(event.x()))
@@ -52,22 +54,29 @@ class UIInitializer:
         selected_shape: str = self.root_widget.layoutComboBox.currentText()
         selected_orientation: str = self.root_widget.orientationComboBox.currentText()
         orientation_value = self.orientations.get(selected_orientation)
+        width_value = int(self.root_widget.gridWidthBox.value())
+        height_value = int(self.root_widget.gridHeightBox.value())
 
-        if self.hexagon_str == selected_shape:
-            self.grid_control = self.builder.build_hexagon(orientation_value, 20, 9, 9)
-        elif self.triangle_str == selected_shape:
-            self.grid_control = self.builder.build_triangle(orientation_value, 20, 10, 10)
-        elif self.trapezoid_str == selected_shape:
-            self.grid_control = self.builder.build_trapezoid(orientation_value, 20, 10, 10)
-        else:
-            self.grid_control = self.builder.build_rectangle(orientation_value, 20, 10, 10)
+        try:
+            if self.hexagon_str == selected_shape:
+                self.grid_control = self.builder.build_hexagon(orientation_value, 20, width_value, height_value)
+            elif self.triangle_str == selected_shape:
+                self.grid_control = self.builder.build_triangle(orientation_value, 20, width_value, height_value)
+            elif self.trapezoid_str == selected_shape:
+                self.grid_control = self.builder.build_trapezoid(orientation_value, 20, width_value, height_value)
+            else:
+                self.grid_control = self.builder.build_rectangle(orientation_value, 20, width_value, height_value)
 
-        hexagon: HexagonImpl
-        scene: QGraphicsScene = QGraphicsScene()
-        for hexagon in self.grid_control.hex_grid.hexagons:
-            points: list[Point] = hexagon.calculate_points(hexagon.calculate_center())
-            poly = QPolygonF()
-            for point in points:
-                poly.append(QPointF(point.coordX, point.coordY))
-            scene.addPolygon(poly)
-        self.root_widget.canvas.setScene(scene)
+            hexagon: HexagonImpl
+            scene: QGraphicsScene = QGraphicsScene()
+            for hexagon in self.grid_control.hex_grid.hexagons:
+                points: list[Point] = hexagon.calculate_points(hexagon.calculate_center())
+                poly = QPolygonF()
+                for point in points:
+                    poly.append(QPointF(point.coordX, point.coordY))
+                scene.addPolygon(poly)
+            self.root_widget.canvas.setScene(scene)
+            self.root_widget.statusBar().clearMessage()
+        except GridBuildException as ex:
+            print("Exception: ", ex.message)
+            self.root_widget.statusBar().showMessage(ex.message, 10000)
